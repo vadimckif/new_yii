6a0b2fb7cfae006564a86ffea82788af9929e49f
Vadim Ananev <ananev.vadim@gmail.com>
2020-02-12 00:39:13 +0200

    first commit yii

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=db19.freehost.com.ua;dbname=bsl_apollo',
+    'username' => 'bsl_9431505',
+    'password' => 'RXLr4asgA',
+    'charset' => 'utf8',
+	'tablePrefix'=>'oc_'
+	//'enableSchemaCache' => true,
+];
